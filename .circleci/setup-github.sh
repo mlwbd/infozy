40661f700be953d7ed978ac4c0e02679b73ec9d0
Sudharshan TK <tksudharshan@gmail.com>
2020-02-10 16:43:44 +0530

    Update setup-github.sh

--- .circleci/setup-github.sh
+++ .circleci/setup-github.sh
@@ -2,7 +2,7 @@ git config user.name "tks18"
 git config user.email "tksudharshan@gmail.com"
 
 git checkout site
-git pull origin site
+git pull origin master
 
 find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
 mv _site/* .
